61a1f0264e34825a823edcea97b8b530fe69e405
Nabin Shrestha <nshrestha@msn.com>
2022-10-14 12:57:02 -0500

    added producers and consumers project modules

---
+++ springboot-kafka-producers/src/main/java/com/nshrestha/skp/SpringbootProducerDemo.java
@@ -0,0 +1,11 @@
+package com.nshrestha.skp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerDemo {
+       public static void main(String[] args) {
+           SpringApplication.run(SpringbootProducerDemo.class, args);
+       }
+}
